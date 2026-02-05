1b638a795cf143368cf566d483e59a15c99f50d7
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2026-02-05 15:03:38 +0400

    feat: add 'Best Staying Places' section with a carousel of `StayingCard` components to display popular tours.

--- client/src/components/Home/Staying.jsx
+++ client/src/components/Home/Staying.jsx
@@ -1,9 +1,34 @@
+import { useContext } from "react"
+import { ToursContext } from "@/context/ToursContext"
+import { Spinner } from "../ui/spinner"
+import StayingCard from "./StayingCard"
+import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from "../ui/carousel"
+
 const Staying = () => {
+
+    const { popularTours, loading } = useContext(ToursContext)
     return (
-        <div className="bg-[#f7f9fc] pt-20">
+        <div className="bg-[#f7f9fc] pt-20 flex flex-col items-center">
             <div className="flex flex-col items-center gap-5">
                 <h3 className="font-medium  text-gray-600  text-2xl"><i>Best Staying Places</i></h3>
                 <h2 className="text-gray-800 font-bold text-5xl"> Best Staying Places</h2>
+            </div>
+            <div className="w-10/12 flex justify-center pb-20 pt-15">
+                {loading ? (
+                    <Spinner className="text-orange-500 size-10" />
+                ) : (
+                    <Carousel className="w-full ">
+                        <CarouselContent >
+                            {popularTours.map((tour) => (
+                                <CarouselItem key={tour._id} className="md:basis-1/2 lg:basis-1/3">
+                                    <StayingCard tour={tour} />
+                                </CarouselItem>
+                            ))}
+                        </CarouselContent>
+                        <CarouselPrevious />
+                        <CarouselNext />
+                    </Carousel>
+                )}
             </div>
         </div>
     )
---
+++ client/src/components/Home/StayingCard.jsx
@@ -0,0 +1,19 @@
+const StayingCard = ({ tour }) => {
+    return (
+        <div className={`bg-cover bg-center w-full h-60 rounded-lg overflow-hidden`} style={{ backgroundImage: `url(${tour.image.url})` }}>
+            <div className="bg-black/40 w-full h-full pl-5 pb-5 relative">
+                <span className="absolute top-5 right-5 flex flex-col gap-2">
+                    {tour.badges.map((badge, index) => (
+                        <span key={index} className="font-bold bg-orange-500 text-white px-2 py-1 rounded-full">{badge}</span>
+                    ))}
+                </span>
+                <div className="flex flex-col  justify-end  h-full">
+                    <h2 className=" font-bold text-2xl text-white"> {tour.title}</h2>
+                    <p className="text-white text-xl">${tour.price.current}/Ticket</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default StayingCard
