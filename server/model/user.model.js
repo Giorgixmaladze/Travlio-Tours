8473ad21b1177357ef691b5b7f7f4e051efc78d2
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2026-03-05 15:19:01 +0400

    make user info unique

--- server/model/user.model.js
+++ server/model/user.model.js
@@ -3,11 +3,13 @@ const mongoose = require("mongoose")
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true, "Name is required"]
+        required: [true, "Name is required"],
+        unique: true
     },
     email: {
         type: String,
-        required: [true, "Email is required"]
+        required: [true, "Email is required"],
+        unique: true
     },
     password: {
         type: String,
