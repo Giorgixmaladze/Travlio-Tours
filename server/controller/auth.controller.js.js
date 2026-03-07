e70b180148344d5425c2a8884053de4b42792440
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2026-03-07 17:05:40 +0400

    feat: Implement user authentication including user registration, login, and email verification.

--- server/controller/auth.controller.js.js
+++ server/controller/auth.controller.js.js
@@ -45,7 +45,7 @@ const createUser = async (req, res, next) => {
         const url = `${req.protocol}://${req.get("host")}/api/auth/register/verify/${code}`;
 
 
-        sendWelcomeEmail(user.email, user.name, url)
+        await sendWelcomeEmail(user.email, user.name, url)
 
         res.status(200).json({
             success: true,
