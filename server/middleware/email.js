0b0c809d06396acf1c4c4492a316f451e8df4c4e
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2026-03-15 19:00:42 +0400

    feat: Add email middleware and a script to test OAuth2 Gmail integration.

--- server/middleware/email.js
+++ server/middleware/email.js
@@ -1,15 +1,12 @@
 const nodemailer = require("nodemailer")
-
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-        type: "OAuth2",
         user: process.env.EMAIL_ADDRESS,
-        clientId: process.env.GMAIL_CLIENT_ID,
-        clientSecret: process.env.GMAIL_CLIENT_SECRET,
-        refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+        pass: process.env.EMAIL_API, // Use App Password instead of OAuth
     },
 });
+
 
 
 
