c1b63881ce4fef1a85dfbe3b17dae4ea92c57025
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2026-04-19 13:10:06 +0400

    feat: create User mongoose model with bcrypt password hashing

--- server/model/user.model.js
+++ server/model/user.model.js
@@ -55,9 +55,6 @@ userSchema.methods.comparePassword = async function (candidatePassword) {
     return await bcrypt.compare(candidatePassword, this.password)
 }
 
-<<<<<<< HEAD
-=======
 
->>>>>>> main
 const User = mongoose.model("User", userSchema)
 module.exports = User
